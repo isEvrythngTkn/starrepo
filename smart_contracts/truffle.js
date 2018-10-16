5d3c8222e2105c1881d89d0ef00621737d6829ea
IsEvrythngTkn <paulngoertzen@gmail.com>
2018-10-16 13:38:18 -0700

    can interact with contract via web3

--- smart_contracts/truffle.js
+++ smart_contracts/truffle.js
@@ -4,11 +4,16 @@ const { mnemonic, infuraURL } = require('./secrets.js');
 
 module.exports = {
   networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
     rinkeby: {
       provider: function() {
         return new HDWalletProvider(mnemonic, infuraURL)
       },
       network_id: 4
-    }   
+    }
   }
 };
